f042060b5351f8ff862b1d19bbbfcfb237b1ce91
Trevor Sears <trevorsears.main@gmail.com>
2019-07-14 15:23:28 -0400

    Removed unused readline dependency for use in browser.

--- js/s2-api-response.js
+++ js/s2-api-response.js
@@ -1,20 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const s2_api_1 = require("./s2-api");
-class S2APIResponse {
-    constructor(response, statusCode) {
-        this.response = response;
-        this.statusCode = statusCode;
-    }
-    getBody() {
-        if (s2_api_1.S2API.isJSON(this.response))
-            return JSON.parse(this.response);
-        else
-            return this.response;
-    }
-    getStatusCode() {
-        return this.statusCode;
-    }
-}
-exports.S2APIResponse = S2APIResponse;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const s2_api_1=require("./s2-api");class S2APIResponse{constructor(s,e){this.response=s,this.statusCode=e}getBody(){return s2_api_1.S2API.isJSON(this.response)?JSON.parse(this.response):this.response}getStatusCode(){return this.statusCode}}exports.S2APIResponse=S2APIResponse;
 //# sourceMappingURL=s2-api-response.js.map
